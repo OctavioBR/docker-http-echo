6e3d5a2eccee4130a0cb619a570227c007b074f3
Octavio Richter <octavio.richter@fundingcircle.com>
2023-04-16 22:32:00 +0100

    Fix terraform fmt

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,9 +9,9 @@ terraform {
   }
 
   backend "s3" {
-    region   = "eu-west-2"
-    bucket   = "097614841487-tfstate"
-    key      = "docker-http-echo.tfstate"
+    region = "eu-west-2"
+    bucket = "097614841487-tfstate"
+    key    = "docker-http-echo.tfstate"
   }
 }
 
